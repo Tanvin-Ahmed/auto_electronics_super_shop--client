3d7a7bb7a51b0c42c36cfbc8380972da39af525e
Tanvin Ahmed <tanvinahmedtouhid91@gmail.com>
2022-03-21 08:26:39 +0600

    update firebase config and json file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCqyE_AgtX_PiXAlrXdm-T-oymQLqKqX9w",
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: "auto-electronics-4e875.firebaseapp.com",
 	projectId: "auto-electronics-4e875",
 	storageBucket: "auto-electronics-4e875.appspot.com",
-	messagingSenderId: "126535305493",
-	appId: "1:126535305493:web:07819d46eddcee3317811c",
-	measurementId: "G-3Q962CJ6SP",
+	messagingSenderId: process.env.REACT_APP_FIREBASE_PROJECT_MESSING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
