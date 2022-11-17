9258a6049c4d44c4aa05035a77d4cfd915c02717
Tanvin Ahmed <tanvinahmedtouhid91@gmail.com>
2022-11-17 15:56:12 +0600

    add style in product card

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,28 +5,28 @@ import Rating from "../Rating/Rating";
 import "./Product.scss";
 
 const Product = ({ product }) => {
-	return (
-		<Card className="my-3 p-3 rounded product">
-			<Link to={`/product/${product._id}`}>
-				<Card.Img src={product.image} variant="top" />
-			</Link>
-			<Card.Body>
-				<Link to={`/product/${product._id}`}>
-					<Card.Title as="div">
-						<strong>{product.name}</strong>
-					</Card.Title>
-				</Link>
+  return (
+    <Card className="my-3 p-3 rounded product h-100">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
 
-				<Card.Text as="div">
-					<Rating
-						value={product.rating}
-						text={`${product.numReviews} reviews`}
-					/>
-				</Card.Text>
-				<Card.Text as="h3">${product.price}</Card.Text>
-			</Card.Body>
-		</Card>
-	);
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Product;
